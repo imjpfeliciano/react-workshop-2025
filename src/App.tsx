2f34bf97fb71d82d6df72169759777fcb80eba71
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2025-02-18 17:36:18 -0600

    feat: add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <div className='h-1/2 flex flex-row gap-4'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,9 +15,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className='text-2xl'>Vite + React</h1>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <button onClick={() => setCount((count) => count + 1)} className='px-2 py-1 bg-slate-200  rounded border border-slate-300'>
           count is {count}
         </button>
         <p>
@@ -28,7 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
