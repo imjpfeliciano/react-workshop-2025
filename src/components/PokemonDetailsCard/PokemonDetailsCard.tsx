db38ebe7ae4dbaca7769ea366349bd426d80993f
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2025-02-21 11:57:15 -0600

    refactor: move pokemon query by id to custom hook

--- src/components/PokemonDetailsCard/PokemonDetailsCard.tsx
+++ src/components/PokemonDetailsCard/PokemonDetailsCard.tsx
@@ -1,43 +1,20 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { usePokemonQuery } from "../../queries/usePokemonQuery";
 import PokemonCard from "../PokemonCard/PokemonCard";
 
-interface PokemonDetailsCardProps {}
-
-const PokemonDetailsCard: React.FC<PokemonDetailsCardProps> = () => {
+const PokemonDetailsCard: React.FC = () => {
   const { pokemonId } = useParams();
-  const [pokemon, setPokemon] = useState(null);
-
-  useEffect(() => {
-    const fetchPokemonDetails = async () => {
-      const pokemonResponse = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
-      );
-      const data = await pokemonResponse.json();
-
-      setPokemon(data);
-    };
-
-    if (pokemonId === undefined) {
-      return;
-    }
-
-    fetchPokemonDetails();
-  }, [pokemonId]);
+  const { isLoading, data: pokemon } = usePokemonQuery(pokemonId!);
 
   return (
     <div className="w-full">
       <div>Pokemon details for id: {pokemonId}</div>
 
+      {isLoading && <div>Loading...</div>}
+
       {pokemon && (
         <div className="w-[500px]">
-          <PokemonCard
-            pokemon={{
-              id: pokemon.id,
-              name: pokemon.name,
-              type: pokemon.types.map(({ type }) => type.name),
-            }}
-          />
+          <PokemonCard pokemon={pokemon} />
         </div>
       )}
     </div>
---
+++ src/queries/usePokemonQuery.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { Pokemon } from "../api/pokemons";
+
+const fetchPokemonById = async (pokemonId: string): Promise<Pokemon> => {
+  const pokemonResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+  );
+  const data = await pokemonResponse.json();
+
+  return {
+    id: data.id,
+    name: data.name,
+    type: data.types.map(({ type }) => type.name),
+  };
+};
+
+export const usePokemonQuery = (pokemonId: string) => {
+  return useQuery({
+    queryKey: ["pokemon", pokemonId],
+    queryFn: () => fetchPokemonById(pokemonId),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
+};
